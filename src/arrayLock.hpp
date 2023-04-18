acba158ed8e4f252fc049dac2f7656164527d98c
zhouzilong <529620861@qq.com>
2023-04-18 10:43:29 -0400

    fix:array lock

--- src/arrayLock.hpp
+++ src/arrayLock.hpp
@@ -10,20 +10,16 @@
 class ArrayLock : public Lock {
    public:
     virtual void lock(const TestContext& ctx) {
-        // atomic circular increment
-        int oldHead = head;
-        int newHead = (oldHead + 1) % threadCnt;
-        while (!head.compare_exchange_weak(oldHead, newHead))
-            ;
-        setMyEle(const_cast<TestContext*>(&ctx), oldHead);
-
-        while (status[oldHead].value.load() == 1)
+        auto myEle = head.fetch_add(1, std::memory_order_acquire) % threadCnt;
+        setMyEle(const_cast<TestContext*>(&ctx), myEle);
+        while (status[myEle].value.load() == 1)
             ;
     }
 
     virtual void unlock(const TestContext& ctx) {
-        status[ctx.myEle].value.store(1);
-        status[(ctx.myEle + 1) % threadCnt].value.store(0);
+        status[ctx.myEle].value.store(1, std::memory_order_release);
+        status[(ctx.myEle + 1) % threadCnt].value.store(
+            0, std::memory_order_release);
     };
 
     virtual std::string getName() { return name; };
@@ -32,8 +28,9 @@ class ArrayLock : public Lock {
         this->threadCnt = threadCnt;
         status = new PaddedInt[threadCnt];
         for (unsigned int i = 0; i < threadCnt; i++) {
-            status[i].value.store(0);
+            status[i].value.store(1);
         }
+        status[0].value.store(0);
     }
 
    private:
